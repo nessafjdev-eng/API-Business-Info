952698c10c4c7d2aa8f0be983c522845056ae715
Vanessa Lima <nessafj.dev@gmail.com>
2026-04-08 23:47:47 -0300

    Criação da tabela RentalPartner

---
+++ BusinessInfo.Domain/Entities/RentalPartner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessInfo.Domain.Entities
+{
+    public class RentalPartner : BaseEntity
+    {
+        public Guid IssuerId { get; set; }
+        public Issuer Issuer { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public bool Active { get; set; }
+    }
+}
